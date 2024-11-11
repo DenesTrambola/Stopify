68e6562cf032fc1d2a7ba2e2386030e3ddabbe09
DenesTrambola <156126997+DenesTrambola@users.noreply.github.com>
2024-11-11 19:25:24 +0200

    Update StopifyDbContext.cs

--- Stopify.Infrastructure/Persistence/StopifyDbContext.cs
+++ Stopify.Infrastructure/Persistence/StopifyDbContext.cs
@@ -27,7 +27,6 @@ public partial class StopifyDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        //                              optionsBuilder.UseSqlServer("Server=tcp:stopify.database.windows.net,1433;Initial Catalog=Stopify;Persist Security Info=False;User ID=tramboladenes;Password=Asd12345;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
         base.OnConfiguring(optionsBuilder);
     }
 
