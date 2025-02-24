5e2ab9915dbf8f0057268bd3440246f299e5d46c
DenesTrambola <156126997+DenesTrambola@users.noreply.github.com>
2025-02-24 14:54:08 +0200

    feature: create ViewModelBase and MainViewModel files

---
+++ Stopify.Presentation/ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel;
+
+namespace Stopify.Presentation.ViewModels.Base;
+
+abstract class ViewModelBase : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+}
