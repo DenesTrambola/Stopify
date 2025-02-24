91baaef55eca05f23264540c9c51d93edffde984
Denes Trambola <156126997+DenesTrambola@users.noreply.github.com>
2025-02-24 14:39:14 +0200

    Revert "refactor: Restructure MVVM folder structure to maintainability"

---
+++ Stopify.Presentation/Views/NowPlayingView/NowPlayingViewItem.xaml.cs
@@ -0,0 +1,132 @@
+﻿using Stopify.Presentation.Helpers.Animations;
+using Stopify.Presentation.Helpers.Utilities;
+using Stopify.Presentation.Views.ArtistView;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Stopify.Presentation.Views.NowPlayingView;
+
+public partial class NowPlayingViewItem : UserControl
+{
+    private MainWindow _mainWindow = (MainWindow)Application.Current.MainWindow;
+    private TextBlock _popupText = new();
+    private bool _isPlaying = false;
+
+    public NowPlayingViewItem()
+    {
+        InitializeComponent();
+
+        _popupText.Foreground = Brushes.White;
+        _popupText.Background = Brushes.Transparent;
+        _popupText.FontWeight = FontWeights.SemiBold;
+        _popupText.FontSize = 14;
+    }
+
+
+    // Now Playing Item
+
+    private void ItemBtn_MouseEnter(object sender, MouseEventArgs e)
+    {
+        Mouse.OverrideCursor = Cursors.Hand;
+        if (!ItemBtn.IsFocused)
+            ColorAnimations.AnimateBackgroundColor(ItemBorder, ItemBorder.Background, Color.FromRgb(31, 31, 31), .1);
+    }
+
+    private void ItemBtn_MouseLeave(object sender, MouseEventArgs e)
+    {
+        Mouse.OverrideCursor = Cursors.Arrow;
+        if (!ItemBtn.IsFocused)
+            ColorAnimations.AnimateBackgroundColor(ItemBorder, ItemBorder.Background, Color.FromRgb(18, 18, 18), .1);
+    }
+
+    private void ItemBtn_Click(object sender, RoutedEventArgs e) =>
+        ItemBtn.Focus();
+
+    private void ItemBtn_GotFocus(object sender, RoutedEventArgs e) =>
+        ColorAnimations.AnimateBackgroundColor(ItemBorder, ItemBorder.Background, Color.FromRgb(42, 42, 42), .05);
+
+    private void ItemBtn_LostFocus(object sender, RoutedEventArgs e) =>
+        ColorAnimations.AnimateBackgroundColor(ItemBorder, ItemBorder.Background, Color.FromRgb(18, 18, 18), .05);
+
+    private void ItemBtn_MouseDoubleClick(object sender, MouseButtonEventArgs e) { }
+
+
+    // Play/Pause
+
+    private void PlayBtn_MouseEnter(object sender, MouseEventArgs e)
+    {
+        ScaleAnimations.BeginScaleAnimation(PlayBtn, 1.03, .05);
+        _popupText.Text = "Play Hope from Lucid Keys";
+        PopupHelper.PopupAppear(_mainWindow, PlayBtn, PlacementMode.Top, _popupText);
+    }
+
+    private void PlayBtn_MouseLeave(object sender, MouseEventArgs e)
+    {
+        ScaleAnimations.ResetScaleAnimation(PlayBtn, .05);
+        PopupHelper.PopupDisappear(_mainWindow);
+    }
+
+    private void PlayBtn_Click(object sender, RoutedEventArgs e)
+    {
+        e.Handled = true;
+
+        if (_isPlaying)
+        {
+            PlayBtn.Content = "\uf04b";
+            PlayBtn.FontSize = 23;
+            _isPlaying = false;
+        }
+        else
+        {
+            PlayBtn.Content = "\uf04c";
+            PlayBtn.FontSize = 25;
+            _isPlaying = true;
+        }
+    }
+
+
+    // Artist
+
+    private void ArtistBtn_MouseEnter(object sender, MouseEventArgs e)
+    {
+        if (ArtistBtn.Content is TextBlock textBlock)
+            textBlock.TextDecorations = TextDecorations.Underline;
+        else if (ArtistBtn.Content is string text)
+        {
+            var newTextBlock = new TextBlock { Text = text, TextDecorations = TextDecorations.Underline };
+            ArtistBtn.Content = newTextBlock;
+        }
+    }
+
+    private void ArtistBtn_MouseLeave(object sender, MouseEventArgs e)
+    {
+        if (ArtistBtn.Content is TextBlock textBlock)
+            textBlock.TextDecorations = null;
+    }
+
+    private void ArtistBtn_Click(object sender, RoutedEventArgs e) =>
+        _mainWindow.MainFrame.Navigate(new ArtistViewMain());
+
+
+    // More Options
+
+    private void OptionsBtn_MouseEnter(object sender, MouseEventArgs e)
+    {
+        ScaleAnimations.BeginScaleAnimation(OptionsBtn, 1.03, .1);
+        ColorAnimations.AnimateForegroundColor(OptionsBtn, OptionsBtn.Foreground, Colors.White, .1);
+        _popupText.Text = "More options for Azahriah";
+        PopupHelper.PopupAppear(_mainWindow, OptionsBtn, PlacementMode.Top, _popupText);
+    }
+
+    private void OptionsBtn_MouseLeave(object sender, MouseEventArgs e)
+    {
+        ScaleAnimations.ResetScaleAnimation(OptionsBtn, .1);
+        ColorAnimations.AnimateForegroundColor(OptionsBtn, OptionsBtn.Foreground, Colors.DarkGray, .1);
+        PopupHelper.PopupDisappear(_mainWindow);
+    }
+
+    private void OptionsBtn_Click(object sender, RoutedEventArgs e) { }
+}
