5457abf73fc69e8b00c1348b3cf3ee3d482180eb
DenesTrambola <156126997+DenesTrambola@users.noreply.github.com>
2025-03-01 18:39:37 +0200

    feature: implement SidebarControlViewModel and SidebarItemViewModel listing

---
+++ Stopify.Presentation/ViewModels/Sidebar/SidebarItemViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Stopify.Presentation.ViewModels.Base;
+
+namespace Stopify.Presentation.ViewModels.Sidebar;
+
+public class SidebarItemViewModel : ViewModelBase
+{
+    private string _title;
+    private string _description;
+    private string _image;
+
+    public string Title
+    {
+        get => _title;
+        set => SetProperty(ref _title, value);
+    }
+
+    public string Description
+    {
+        get => _description;
+        set => SetProperty(ref _description, value);
+    }
+
+    public string Image
+    {
+        get => _image;
+        set => SetProperty(ref _image, value);
+    }
+
+    public SidebarItemViewModel(string title, string description, string image)
+    {
+        Title = title;
+        Description = description;
+        Image = image;
+    }
+}
